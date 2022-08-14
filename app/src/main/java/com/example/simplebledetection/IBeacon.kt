c39be6a49af504dd7aa59ca0cde3dc8cd664fea3
tranchikhang <tranchikhang@outlook.com>
2022-08-14 17:06:19 +0900

    refactor ibeacon class

--- app/src/main/java/com/example/simplebledetection/IBeacon.kt
+++ app/src/main/java/com/example/simplebledetection/IBeacon.kt
@@ -1,6 +1,5 @@
 package com.example.simplebledetection
 
-import android.util.Log
 
 class IBeacon {
 
@@ -12,23 +11,23 @@ class IBeacon {
     /**
      * The measured signal strength of the Bluetooth packet
      */
-    protected var rssi: Int = 0
+    private var rssi: Int = 0
 
     /**
      * packet raw data
      */
-    protected var rawByteData: ByteArray = ByteArray(30)
+    private var rawByteData: ByteArray = ByteArray(30)
 
     /**
      * major minor, and their position (based on iBeacon specs)
      */
-    private var major: Int = -1
-    protected val majorPosStart = 25
-    protected val majorPosEnd = 26
+    private var major: Int? = null
+    private val majorPosStart = 25
+    private val majorPosEnd = 26
 
-    private var minor: Int = -1
-    protected val minorPosStart = 27
-    protected val minorPosEnd = 28
+    private var minor: Int? = null
+    private val minorPosStart = 27
+    private val minorPosEnd = 28
 
     private val TAG = "IBeacon"
 
@@ -43,9 +42,6 @@ class IBeacon {
                 val uuidBytes = ByteArray(16)
                 System.arraycopy(rawByteData, startByte + 4, uuidBytes, 0, 16)
                 val hexString = BLEDevice.bytesToHex(uuidBytes)
-                // 0x020x010x1A0x1A0xFF0x4C0x000x020x150xE80xC60x560x020x6D0x9C0x440xEF0x970x340xB20xD30xEF0x1C0xD90x610x000x010x070x210xC50x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x000x00
-                Log.d(TAG, hexString)
-                val sb = StringBuilder()
                 if (!hexString.isNullOrEmpty()) {
                     uuid = hexString.substring(0, 8) + "-" +
                             hexString.substring(8, 12) + "-" +
@@ -60,17 +56,21 @@ class IBeacon {
 
     fun getUUID(): String {
         if (uuid.isNullOrEmpty()) {
-            parseUUID();
+            parseUUID()
         }
         return uuid
     }
 
     fun getMajor(): Int {
-        return (rawByteData[majorPosStart].toInt() and 0xff) * 0x100 + (rawByteData[majorPosEnd].toInt() and 0xff)
+        if (major == null)
+            major = (rawByteData[majorPosStart].toInt() and 0xff) * 0x100 + (rawByteData[majorPosEnd].toInt() and 0xff)
+        return major as Int
     }
 
     fun getMinor(): Int {
-        return (rawByteData[minorPosStart].toInt() and 0xff) * 0x100 + (rawByteData[minorPosEnd].toInt() and 0xff)
+        if (minor == null)
+            minor = (rawByteData[minorPosStart].toInt() and 0xff) * 0x100 + (rawByteData[minorPosEnd].toInt() and 0xff)
+        return minor as Int
     }
 
 }
